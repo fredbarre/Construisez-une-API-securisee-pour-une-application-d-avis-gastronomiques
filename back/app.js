a077f1e02b9a7463858773798b7c51bde57819c2
fredbarre <fredbarremail@protonmail.com>
2022-07-21 21:02:12 +0200

    installation express mangoose mise en place de tests de fonctionnement

--- back/app.js
+++ back/app.js
@@ -1,9 +1,61 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const userModel = require("./models/user.js");
 
-app.use((req, res) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
+const DBPASSWORD = process.env.DBPASSWORD;
+/*console.log(
+  `mongodb+srv://DatabaseUser:${DBPASSWORD}@cluster0.3nenbtk.mongodb.net/test`
+);*/
+mongoose.connect(
+  `mongodb+srv://DatabaseUser:${DBPASSWORD}@cluster0.3nenbtk.mongodb.net/test`,
+  function (err) {
+    if (err) {
+      throw err;
+    }
+  }
+);
+
+//let userModel = mongoose.model("user", userSchema);
+let user = new userModel({ username: "bob", email: "bob@mail.com" });
+
+user.save(function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log("utilisateur ajouté avec succès !");
 });
 
+app.post("/api/stuff", (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body,
+  });
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Requête reçue !");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: "Votre requête a bien été reçue !" });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Réponse envoyée avec succès !");
+});
 module.exports = app;
