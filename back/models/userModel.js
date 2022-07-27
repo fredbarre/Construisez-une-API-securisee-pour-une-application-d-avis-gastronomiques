0a5f45123f5a009fc600926148566461daf9eb28
fredbarre <fredbarremail@protonmail.com>
2022-07-27 17:00:27 +0200

    utilisation du plugin unique validator et email unique

--- back/models/userModel.js
+++ back/models/userModel.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
+userSchema.plugin(uniqueValidator);
 
 let userModel = mongoose.model("user", userSchema);
 module.exports = userModel;
