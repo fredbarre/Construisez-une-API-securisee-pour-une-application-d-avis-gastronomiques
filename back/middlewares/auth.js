2eaf2e909e5a65886571e8315cbf6278b4301b85
fredbarre <fredbarremail@protonmail.com>
2022-07-28 07:21:30 +0200

    creation middleware d'autoriasation

---
+++ back/middlewares/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const TOKENSECRET = process.env.TOKENSECRET;
+ 
+module.exports = (req, res, next) => {
+   try {
+       const token = req.headers.authorization.split(' ')[1];
+       const decodedToken = jwt.verify(token, TOKENSECRET);
+       const userId = decodedToken.userId;
+       req.auth = {
+           userId: userId
+       };
+	next();
+   } catch(error) {
+       res.status(401).json({ error });
+   }
+};
