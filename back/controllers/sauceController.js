16c372817dc9a347326c162a761493d4e8424594
fredbarre <fredbarremail@protonmail.com>
2022-07-29 08:19:06 +0200

    création de sauceController

---
+++ back/controllers/sauceController.js
@@ -0,0 +1,15 @@
+const mongodb = require("../managers/mongoDB");
+
+let sauce_Controller = require("../models/sauceModel");
+
+exports.getSauces = function (req, res) { };
+
+exports.getSauce = function (req, res){ };
+
+exports.newSauce = function(req, res){};
+
+exports.updateSauce = function (req, res){};
+
+exports.deleteSauce = function (req, res){};
+
+exports.setLike = function (req, res){};
