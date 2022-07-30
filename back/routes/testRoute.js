6a63da54812be1e2369bb4714030db6b402f4a71
fredbarre <fredbarremail@protonmail.com>
2022-07-30 17:41:17 +0200

    création de testRoute pour effectuer les test d'authorisation et test multer-config

---
+++ back/routes/testRoute.js
@@ -0,0 +1,11 @@
+let express = require("express");
+let router = express.Router();
+
+let auth = require("../middlewares/auth");
+router.get("/auth", auth, (req, res) => {
+  res.status(201).json(req.auth);
+});
+
+//router.get("/", user_controller.login);
+
+module.exports = router;
