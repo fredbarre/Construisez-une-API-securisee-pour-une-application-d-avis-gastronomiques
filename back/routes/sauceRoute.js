91982e6e2e6dcc0be1d4508e34090ebf7f201418
fredbarre <fredbarremail@protonmail.com>
2022-08-01 19:31:20 +0200

    ajout auth et multer

--- back/routes/sauceRoute.js
+++ back/routes/sauceRoute.js
@@ -1,19 +1,19 @@
-let express = require("express");
-let router = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const auth = require("../middlewares/auth");
+const multer = require("../middlewares/multer-config");
 let sauce_controller = require("../controllers/sauceController");
 
-router.get("/api/sauces", sauce_controller.getSauces);
+router.get("/api/sauces", auth, sauce_controller.getSauces);
 
-router.get("/api/sauces/:id", sauce_controller.getSauce);
+router.get("/api/sauces/:id", auth, sauce_controller.getSauce);
 
-router.post("/api/sauces", sauce_controller.newSauce);
+router.post("/api/sauces", auth, multer, sauce_controller.newSauce);
 
-router.put("/api/sauces/:id", sauce_controller.updateSauce);
+router.put("/api/sauces/:id", auth, multer, sauce_controller.updateSauce);
 
-router.delete("/api/sauces/:id", sauce_controller.deleteSauce);
+router.delete("/api/sauces/:id", auth, sauce_controller.deleteSauce);
 
-router.post("/api/sauces/:id/like", sauce_controller.setLike);
-
+router.post("/api/sauces/:id/like", auth, sauce_controller.setLike);
 
 module.exports = router;
