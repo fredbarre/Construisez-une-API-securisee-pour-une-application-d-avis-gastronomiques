6c2b2d2421337c9c394b7a12b69b5d18251a977a
fredbarre <fredbarremail@protonmail.com>
2022-08-07 17:42:52 +0200

    ajout d'un test pour multer

--- back/routes/testRoute.js
+++ back/routes/testRoute.js
@@ -2,10 +2,15 @@ let express = require("express");
 let router = express.Router();
 
 let auth = require("../middlewares/auth");
+const multerMW = require("../middlewares/multer-config");
 router.get("/auth", auth, (req, res) => {
   res.status(201).json(req.auth);
 });
 
-//router.get("/multer", user_controller.login);
+router.get("/multer", multerMW, (req, res) => {
+  console.log(req);
+  const { file, body } = req;
+  res.status(201).json({ file, body });
+});
 
 module.exports = router;
