a442586fe7e0ac351b3d381026dc67ac93208ff0
fredbarre <fredbarremail@protonmail.com>
2022-08-10 12:55:13 +0200

    ajout de ressources statique images

--- back/app.js
+++ back/app.js
@@ -21,6 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev", { immediate: false }));
 app.use(morgan("dev", { immediate: true }));
 app.use(require("./middlewares/cors"));
+app.use(express.static("images"));
 app.use("/", require("./routes/userRoute"));
 app.use("/", require("./routes/sauceRoute"));
 //app.use("/images", express.static(path.join(__dirname, "images")));
